3e5a318d0cb157bea90ac444f8d54654f94a2209
Jef King <jefkin@microsoft.com>
2016-02-05 21:28:07 -0800

    boom

--- Apps/TrapDropp/TrapDropp/www/scripts/app.js
+++ Apps/TrapDropp/TrapDropp/www/scripts/app.js
@@ -1,1 +1,9 @@
-﻿
+﻿'use strict';
+
+var ratApp = angular.module('ratApp', []);
+
+nutritionApp.controller('nutrition', ['$scope', '$http', function ($scope, $http) {
+    $http.get('Scripts/data.js').success(function (data) {
+        $scope.foods = data;
+    });
+}]);

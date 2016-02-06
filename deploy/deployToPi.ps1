9fad555a67dcfa4cd7a954c0803c529149f43fbd
Julien Stroheker <julienstroheker@gmail.com>
2016-02-06 12:53:48 -0800

    Updating deploy script

--- deploy/deployToPi.ps1
+++ deploy/deployToPi.ps1
@@ -5,6 +5,6 @@ net use w: \\mousetrapp\c$\MouseTrapp\ /user:Administrator C@nITPr01
 write-host "W drive Mount" 
 Remove-Item W:\deploy\* -Recurse
 
-xcopy C:\Data\Hackathon\MouseTrapp\MouseTrapp.IOT\MouseTrapp.IOT\AppPackages\MouseTrapp.IOT_2.0.0.0_ARM_Test W:\deploy /i /s
+xcopy C:\Data\Hackathon\agent\_work\1\b\AppxPackages\MouseTrapp.IOT_2.0.0.0_ARM_Test W:\deploy /i /s
 
 Invoke-Command -ComputerName mousetrapp -ScriptBlock { ping google.com } -credential $mycreds

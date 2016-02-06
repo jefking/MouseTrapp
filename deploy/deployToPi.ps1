7df256f4ed948611e52b113bcace4e5f31bff7cb
Julien Stroheker <julienstroheker@gmail.com>
2016-02-06 12:51:23 -0800

    Updating deploy script

--- deploy/deployToPi.ps1
+++ deploy/deployToPi.ps1
@@ -2,9 +2,9 @@ $secpasswd = ConvertTo-SecureString "C@nITPr01" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("Administrator", $secpasswd)
 
 net use w: \\mousetrapp\c$\MouseTrapp\ /user:Administrator C@nITPr01
-
+write-host "W drive Mount" 
 Remove-Item W:\deploy\* -Recurse
 
-xcopy C:\Data\Hackathon\MouseTrapp\MouseTrapp.IOT\MouseTrapp.IOT\AppPackages\MouseTrapp.IOT_1.0.2.0_ARM_Test W:\deploy /i /s
+xcopy C:\Data\Hackathon\MouseTrapp\MouseTrapp.IOT\MouseTrapp.IOT\AppPackages\MouseTrapp.IOT_2.0.0.0_ARM_Test W:\deploy /i /s
 
 Invoke-Command -ComputerName mousetrapp -ScriptBlock { ping google.com } -credential $mycreds

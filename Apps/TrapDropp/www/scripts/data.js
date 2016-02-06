59406e5ac83fcc65003986b3efe6d3783a799fa2
Jef King <jefkin@microsoft.com>
2016-02-06 09:23:22 -0800

    move

---
+++ Apps/TrapDropp/www/scripts/data.js
@@ -0,0 +1,711 @@
+﻿[
+    {
+
+        "Name": "Olive Oil",
+        "Fat": 93.33333333333334,
+        "Carb": 0,
+        "Protein": 0,
+        "Calories": 800
+    },
+    {
+
+        "Name": "Extra Virgin Olive Oil",
+        "Fat": 100,
+        "Carb": 0,
+        "Protein": 0,
+        "Calories": 884
+    },
+    {
+
+        "Name": "Macadamia",
+        "Fat": 75.8,
+        "Carb": 13.8,
+        "Protein": 7.9,
+        "Calories": 740
+    },
+    {
+
+        "Name": "Clarified Butter",
+        "Fat": 82.1,
+        "Carb": 0,
+        "Protein": 0,
+        "Calories": 726
+    },
+    {
+
+        "Name": "Pili Nut",
+        "Fat": 79,
+        "Carb": 4.2,
+        "Protein": 10.8,
+        "Calories": 719.2
+    },
+    {
+
+        "Name": "Savi Seed Carmalized",
+        "Fat": 32,
+        "Carb": 42.9,
+        "Protein": 21.4,
+        "Calories": 714
+    },
+    {
+
+        "Name": "Pecan nuts",
+        "Fat": 72,
+        "Carb": 14,
+        "Protein": 9,
+        "Calories": 690
+    },
+    {
+        "Name": "Pine Nuts",
+        "Fat": 68,
+        "Carb": 13,
+        "Protein": 14,
+        "Calories": 673
+    },
+    {
+
+        "Name": "Brazil nuts",
+        "Fat": 66,
+        "Carb": 12,
+        "Protein": 14,
+        "Calories": 656
+    },
+    {
+
+        "Name": "Walnut",
+        "Fat": 65,
+        "Carb": 14,
+        "Protein": 15,
+        "Calories": 654
+    },
+    {
+
+        "Name": "Hazel Nuts",
+        "Fat": 61,
+        "Carb": 17,
+        "Protein": 15,
+        "Calories": 628
+    },
+    {
+
+        "Name": "WowButter (Soy)",
+        "Fat": 46.9,
+        "Carb": 25,
+        "Protein": 21.9,
+        "Calories": 625
+    },
+    {
+
+        "Name": "Kraft Crunchy Peanut Butter",
+        "Fat": 50,
+        "Carb": 20,
+        "Protein": 25,
+        "Calories": 600
+    },
+    {
+
+        "Name": "Toasted Coconut",
+        "Fat": 47,
+        "Carb": 44,
+        "Protein": 5,
+        "Calories": 592
+    },
+    {
+
+        "Name": "Sunflower Seeds",
+        "Fat": 51,
+        "Carb": 20,
+        "Protein": 21,
+        "Calories": 584
+    },
+    {
+
+        "Name": "Hemp Seeds/Hearts",
+        "Fat": 46.7,
+        "Carb": 6.7,
+        "Protein": 36.7,
+        "Calories": 580
+    },
+    {
+
+        "Name": "Almond",
+        "Fat": 49,
+        "Carb": 22,
+        "Protein": 21,
+        "Calories": 576
+    },
+    {
+
+        "Name": "Sesame seeds",
+        "Fat": 50,
+        "Carb": 23,
+        "Protein": 18,
+        "Calories": 573
+    },
+    {
+
+        "Name": "Peanuts",
+        "Fat": 49,
+        "Carb": 16,
+        "Protein": 26,
+        "Calories": 567
+    },
+    {
+
+        "Name": "Pistachio",
+        "Fat": 45,
+        "Carb": 29,
+        "Protein": 21,
+        "Calories": 567
+    },
+    {
+
+        "Name": "Cashew",
+        "Fat": 44,
+        "Carb": 30,
+        "Protein": 18,
+        "Calories": 553
+    },
+    {
+
+        "Name": "Yogurt covered almonds",
+        "Fat": 35,
+        "Carb": 55,
+        "Protein": 10,
+        "Calories": 550
+    },
+    {
+
+        "Name": "Dark Chocolate",
+        "Fat": 31,
+        "Carb": 61,
+        "Protein": 4.9,
+        "Calories": 546
+    },
+    {
+
+        "Name": "White chocolate",
+        "Fat": 32.4,
+        "Carb": 59.4,
+        "Protein": 5.9,
+        "Calories": 538
+    },
+    {
+
+        "Name": "Flax seeds",
+        "Fat": 42,
+        "Carb": 29,
+        "Protein": 18,
+        "Calories": 534
+    },
+    {
+
+        "Name": "Nature Valley Granola Thins",
+        "Fat": 23.6,
+        "Carb": 23.5,
+        "Protein": 5.9,
+        "Calories": 529
+    },
+    {
+
+        "Name": "Nutella",
+        "Fat": 32.4,
+        "Carb": 56.8,
+        "Protein": 5.4,
+        "Calories": 526
+    },
+    {
+
+        "Name": "Poppy seeds",
+        "Fat": 42,
+        "Carb": 28,
+        "Protein": 18,
+        "Calories": 525
+    },
+    {
+
+        "Name": "Milk Chocolate covered Peanuts",
+        "Fat": 42.5,
+        "Carb": 37.5,
+        "Protein": 15,
+        "Calories": 525
+    },
+    {
+
+        "Name": "Banana Chips",
+        "Fat": 34,
+        "Carb": 58,
+        "Protein": 2.3,
+        "Calories": 519
+    },
+    {
+
+        "Name": "HobNob Chocolate Cookies",
+        "Fat": 21.4,
+        "Carb": 62,
+        "Protein": 7.9,
+        "Calories": 500
+    },
+    {
+
+        "Name": "Short Bread",
+        "Fat": 24,
+        "Carb": 64,
+        "Protein": 6,
+        "Calories": 500
+    },
+    {
+
+        "Name": "Soy Nuts",
+        "Fat": 26,
+        "Carb": 32,
+        "Protein": 34,
+        "Calories": 500
+    },
+    {
+
+        "Name": "Dried Milk",
+        "Fat": 27,
+        "Carb": 38,
+        "Protein": 26,
+        "Calories": 496
+    },
+    {
+
+        "Name": "Chia seeds",
+        "Fat": 31,
+        "Carb": 42,
+        "Protein": 17,
+        "Calories": 486
+    },
+    {
+
+        "Name": "M&M's",
+        "Fat": 21.4,
+        "Carb": 71.4,
+        "Protein": 4.3,
+        "Calories": 485.7
+    },
+    {
+
+        "Name": "Simple Pleasures Oat Choco",
+        "Fat": 23.3,
+        "Carb": 66.7,
+        "Protein": 6.7,
+        "Calories": 484
+    },
+    {
+        "Name": "Bacon Jerky",
+        "Fat": 32.1,
+        "Carb": 3.6,
+        "Protein": 42.9,
+        "Calories": 480
+    },
+    {
+        "Name": "Granola",
+        "Fat": 20,
+        "Carb": 64,
+        "Protein": 10,
+        "Calories": 471
+    },
+    {
+        "Name": "Parmesan Cheese",
+        "Fat": 30,
+        "Carb": 0,
+        "Protein": 40,
+        "Calories": 466.6
+    },
+    {
+        "Name": "Dipp's Bars",
+        "Fat": 16.1,
+        "Carb": 71,
+        "Protein": 6.5,
+        "Calories": 452
+    },
+    {
+        "Name": "Dried Soybeans",
+        "Fat": 21.5,
+        "Carb": 32.6,
+        "Protein": 39.5,
+        "Calories": 451
+    },
+    {
+        "Name": "Pumpkin Seeds",
+        "Fat": 19,
+        "Carb": 54,
+        "Protein": 19,
+        "Calories": 446
+    },
+    {
+        "Name": "Ramen Noodles",
+        "Fat": 16,
+        "Carb": 63,
+        "Protein": 10,
+        "Calories": 436
+    },
+    {
+        "Name": "Corn nuts",
+        "Fat": 14,
+        "Carb": 72,
+        "Protein": 9,
+        "Calories": 436
+    },
+    {
+        "Name": "Roasted Edemame",
+        "Fat": 13,
+        "Carb": 34,
+        "Protein": 47.8,
+        "Calories": 434
+    },
+    {
+        "Name": "Tsampa",
+        "Fat": 2.7,
+        "Carb": 86.7,
+        "Protein": 11,
+        "Calories": 433.3
+    },
+    {
+        "Name": "Quaker Yogurt Granola Bar",
+        "Fat": 12.9,
+        "Carb": 71.4,
+        "Protein": 5.7,
+        "Calories": 429
+    },
+    {
+        "Name": "Yogurt raisins",
+        "Fat": 17.5,
+        "Carb": 70,
+        "Protein": 2.5,
+        "Calories": 425
+    },
+    {
+        "Name": "Cracker Jack",
+        "Fat": 7.4,
+        "Carb": 85.2,
+        "Protein": 7.4,
+        "Calories": 420
+    },
+    {
+        "Name": "Rice Crispy Squares",
+        "Fat": 8.9,
+        "Carb": 80.5,
+        "Protein": 3.5,
+        "Calories": 409
+    },
+    {
+        "Name": "Whey powder",
+        "Fat": 3.4,
+        "Carb": 10.2,
+        "Protein": 81.6,
+        "Calories": 408
+    },
+    {
+        "Name": "Milk chocolate raisins",
+        "Fat": 22.5,
+        "Carb": 62.5,
+        "Protein": 5,
+        "Calories": 400
+    },
+    {
+        "Name": "Refried Beans",
+        "Fat": 4.5,
+        "Carb": 69,
+        "Protein": 21,
+        "Calories": 390
+    },
+    {
+        "Name": "Rolled Oats",
+        "Fat": 6.2,
+        "Carb": 69,
+        "Protein": 13.6,
+        "Calories": 379
+    },
+    {
+        "Name": "Good Hemp Protein Drink",
+        "Fat": 12.6,
+        "Carb": 15.4,
+        "Protein": 47,
+        "Calories": 370
+    },
+    {
+        "Name": "Quinoa",
+        "Fat": 6,
+        "Carb": 64,
+        "Protein": 14,
+        "Calories": 368
+    },
+    {
+        "Name": "Chick peas",
+        "Fat": 6,
+        "Carb": 61,
+        "Protein": 19,
+        "Calories": 364
+    },
+    {
+        "Name": "Dried mango",
+        "Fat": 0,
+        "Carb": 90.9,
+        "Protein": 0,
+        "Calories": 363.6
+    },
+    {
+        "Name": "Split Red Lentils",
+        "Fat": 1.5,
+        "Carb": 60,
+        "Protein": 28,
+        "Calories": 360
+    },
+    {
+        "Name": "Barley",
+        "Fat": 2.3,
+        "Carb": 73,
+        "Protein": 12,
+        "Calories": 354
+    },
+    {
+        "Name": "Fig Newton",
+        "Fat": 6.5,
+        "Carb": 71,
+        "Protein": 3.2,
+        "Calories": 354
+    },
+    {
+        "Name": "Dried Cherry's",
+        "Fat": 0,
+        "Carb": 82.5,
+        "Protein": 2.5,
+        "Calories": 350
+    },
+    {
+        "Name": "Dried Cantaloupe",
+        "Fat": 0,
+        "Carb": 87.5,
+        "Protein": 2,
+        "Calories": 350
+    },
+    {
+        "Name": "Dried Papaya",
+        "Fat": 0,
+        "Carb": 87.5,
+        "Protein": 0,
+        "Calories": 350
+    },
+    {
+        "Name": "Mung beans",
+        "Fat": 1.2,
+        "Carb": 63,
+        "Protein": 24,
+        "Calories": 347
+    },
+    {
+        "Name": "Buckwheat Groats",
+        "Fat": 2.7,
+        "Carb": 75,
+        "Protein": 12,
+        "Calories": 346
+    },
+    {
+        "Name": "Muesli",
+        "Fat": 4.7,
+        "Carb": 77.6,
+        "Protein": 1.4,
+        "Calories": 340
+    },
+    {
+
+        "Name": "Romano Cheese",
+        "Fat": 26.6,
+        "Carb": 0,
+        "Protein": 33.3,
+        "Calories": 333.3
+    },
+    {
+        "Name": "Cheddar Cheese",
+        "Fat": 26.6,
+        "Carb": 0,
+        "Protein": 20,
+        "Calories": 333.3
+    },
+    {
+        "Name": "Dried Pineapple",
+        "Fat": 0,
+        "Carb": 85.7,
+        "Protein": 2.4,
+        "Calories": 333.3
+    },
+    {
+        "Name": "Beef Jerky",
+        "Fat": 25,
+        "Carb": 10,
+        "Protein": 35,
+        "Calories": 333
+    },
+    {
+        "Name": "Dried Blueberry",
+        "Fat": 2.5,
+        "Carb": 80,
+        "Protein": 2.5,
+        "Calories": 325
+    },
+    {
+        "Name": "Dried Goji berries",
+        "Fat": 0,
+        "Carb": 85.7,
+        "Protein": 14.3,
+        "Calories": 321.4
+    },
+    {
+        "Name": "Dried Cranberry",
+        "Fat": 1.4,
+        "Carb": 82,
+        "Protein": 0.1,
+        "Calories": 308
+    },
+    {
+        "Name": "Raisin",
+        "Fat": 0.5,
+        "Carb": 79,
+        "Protein": 3.1,
+        "Calories": 299
+    },
+    {
+        "Name": "Dried Currants",
+        "Fat": 0,
+        "Carb": 74.3,
+        "Protein": 4.2,
+        "Calories": 283
+    },
+    {
+        "Name": "Dried Date",
+        "Fat": 0.15,
+        "Carb": 75,
+        "Protein": 1.8,
+        "Calories": 277
+    },
+    {
+        "Name": "Dried Apples",
+        "Fat": 0,
+        "Carb": 66.3,
+        "Protein": 1.2,
+        "Calories": 243
+    },
+    {
+        "Name": "Dried Appricot",
+        "Fat": 0.5,
+        "Carb": 63,
+        "Protein": 3.4,
+        "Calories": 241
+    },
+    {
+        "Name": "Dried Prune",
+        "Fat": 0.4,
+        "Carb": 64,
+        "Protein": 2.2,
+        "Calories": 240
+    },
+    {
+        "Name": "Dried Peaches",
+        "Fat": 0.6,
+        "Carb": 61.3,
+        "Protein": 3.8,
+        "Calories": 238.8
+    },
+    {
+        "Name": "Marmite",
+        "Fat": 0.1,
+        "Carb": 19.2,
+        "Protein": 28.4,
+        "Calories": 231
+    },
+    {
+        "Name": "Lima beans",
+        "Fat": 0.4,
+        "Carb": 21,
+        "Protein": 8,
+        "Calories": 115
+    },
+    {
+        "Name": "Vegemite",
+        "Fat": 0,
+        "Carb": 10,
+        "Protein": 12.2,
+        "Calories": 100
+    },
+    {
+        "Name": "Maize/Corn",
+        "Fat": 4.74,
+        "Carb": 74,
+        "Protein": 9.4,
+        "Calories": 1528
+    },
+    {
+        "Name": "Rice (white)",
+        "Fat": 0.66,
+        "Carb": 80,
+        "Protein": 7.1,
+        "Calories": 1528
+    },
+    {
+        "Name": "Rice (brown)",
+        "Fat": 2.92,
+        "Carb": 77,
+        "Protein": 7.9,
+        "Calories": 1549
+    },
+    {
+        "Name": "Wheat",
+        "Fat": 1.54,
+        "Carb": 71,
+        "Protein": 12.6,
+        "Calories": 1369
+    },
+    {
+        "Name": "Potato",
+        "Fat": 0.09,
+        "Carb": 17,
+        "Protein": 2,
+        "Calories": 322
+    },
+    {
+        "Name": "Cassava",
+        "Fat": 0.28,
+        "Carb": 17,
+        "Protein": 1.4,
+        "Calories": 670
+    },
+    {
+        "Name": "Soybean (green)",
+        "Fat": 6.8,
+        "Carb": 11,
+        "Protein": 13,
+        "Calories": 615
+    },
+    {
+        "Name": "Sweet potato",
+        "Fat": 0.05,
+        "Carb": 20,
+        "Protein": 1.6,
+        "Calories": 360
+    },
+    {
+        "Name": "Sorghum",
+        "Fat": 3.3,
+        "Carb": 75,
+        "Protein": 11.3,
+        "Calories": 1419
+    },
+    {
+        "Name": "Yam",
+        "Fat": 0.17,
+        "Carb": 28,
+        "Protein": 1.5,
+        "Calories": 494
+    },
+    {
+        "Name": "Plantain",
+        "Fat": 0.37,
+        "Carb": 32,
+        "Protein": 1.3,
+        "Calories": 511
+    }
+]
